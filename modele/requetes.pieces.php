11e4b373825e2b1f2f0401d5573eec0cbf410cc2
PierreSau <pierre.sauvage@isep.fr>
2019-03-26 15:10:11 +0100

    init MVC

---
+++ modele/requetes.pieces.php
@@ -0,0 +1,25 @@
+
+<?php
+
+/**Fonction table piece*/
+
+// on récupère les requêtes génériques
+include('requetes.generiques.php');
+
+//on définit le nom de la table
+$table = "room";
+
+
+
+/**
+ * Recherche les pieces en fonction du type passé en paramètre
+ * @param PDO $bdd
+ * @param string $table
+ * @param string $type
+ * @return array
+ */
+function rechercheParType(PDO $bdd, string $table, string $type): array {
+
+    return recherche($bdd, $table, ['nom' => $type]);
+
+}
