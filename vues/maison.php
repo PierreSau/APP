871f6226100cefb0592b61d97dded6fbd42bbd5d
LucasDuchassin <49558730+LucasDuchassin@users.noreply.github.com>
2019-04-14 21:56:03 +0100

    ajout maison

---
+++ vues/maison.php
@@ -0,0 +1,436 @@
+<?php
+?>
+
+
+<html>
+<head>
+  <link rel="stylesheet" href="maison.css">
+</head>
+
+
+
+<body>
+<div class="menu">
+
+  <div class="piece">
+    <div class="nomPiece"> Pièce 1 </div>
+    <div class="info"> <img src="info.png" alt="informations" title="Entrer ici les informations à afficher"> </div>
+    <div class="etat">
+      <strong>Capteurs :</strong>
+      <table class="captab">
+        <tr>
+          <th>Capteur</th>
+          <th>Valeur Actuelle</th>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+      </table>
+    </div>
+    <div class="modes">
+      <div class="mode1">Absent</div>
+      <div class="mode2">Eco</div>
+      <div class="mode3">Confort</div>
+    </div>
+    <div class="actionneurs">
+      <strong>Actionneurs</strong>
+        <table class="actab">
+          <tr>
+            <th>Actionneur</th>
+            <th>Valeur actuelle</th>
+            <th>Valeur cible</th>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+        </table>
+    </div>
+    <div class="aide">
+      <img src="info.png" alt="aide concernant les actionneurs" title="Remplir ici le formulaire d'aide">
+    </div>
+  </div>
+
+
+  <div class="piece">
+    <div class="nomPiece"> Pièce 1 </div>
+    <div class="info"> <img src="info.png" alt="informations" title="Entrer ici les informations à afficher"> </div>
+    <div class="etat">
+      <strong>Capteurs :</strong>
+      <table class="captab">
+        <tr>
+          <th>Capteur</th>
+          <th>Valeur Actuelle</th>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+      </table>
+    </div>
+    <div class="modes">
+      <div class="mode1">Absent</div>
+      <div class="mode2">Eco</div>
+      <div class="mode3">Confort</div>
+    </div>
+    <div class="actionneurs">
+      <strong>Actionneurs</strong>
+        <table class="actab">
+          <tr>
+            <th>Actionneur</th>
+            <th>Valeur actuelle</th>
+            <th>Valeur cible</th>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+        </table>
+    </div>
+    <div class="aide">
+      <img src="info.png" alt="aide concernant les actionneurs" title="Remplir ici le formulaire d'aide">
+    </div>
+  </div>
+
+  <div class="piece">
+    <div class="nomPiece"> Pièce 1 </div>
+    <div class="info"> <img src="info.png" alt="informations" title="Entrer ici les informations à afficher"> </div>
+    <div class="etat">
+      <strong>Capteurs :</strong>
+      <table class="captab">
+        <tr>
+          <th>Capteur</th>
+          <th>Valeur Actuelle</th>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+      </table>
+    </div>
+    <div class="modes">
+      <div class="mode1">Absent</div>
+      <div class="mode2">Eco</div>
+      <div class="mode3">Confort</div>
+    </div>
+    <div class="actionneurs">
+      <strong>Actionneurs</strong>
+        <table class="actab">
+          <tr>
+            <th>Actionneur</th>
+            <th>Valeur actuelle</th>
+            <th>Valeur cible</th>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+        </table>
+    </div>
+    <div class="aide">
+      <img src="info.png" alt="aide concernant les actionneurs" title="Remplir ici le formulaire d'aide">
+    </div>
+  </div>
+
+
+  <div class="piece">
+    <div class="nomPiece"> Pièce 1 </div>
+    <div class="info"> <img src="info.png" alt="informations" title="Entrer ici les informations à afficher"> </div>
+    <div class="etat">
+      <strong>Capteurs :</strong>
+      <table class="captab">
+        <tr>
+          <th>Capteur</th>
+          <th>Valeur Actuelle</th>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+        <tr>
+          <td>Nom Capteur</td>
+          <td>Valeur Capteur</td>
+        </tr>
+      </table>
+    </div>
+    <div class="modes">
+      <div class="mode1">Absent</div>
+      <div class="mode2">Eco</div>
+      <div class="mode3">Confort</div>
+    </div>
+    <div class="actionneurs">
+      <strong>Actionneurs</strong>
+        <table class="actab">
+          <tr>
+            <th>Actionneur</th>
+            <th>Valeur actuelle</th>
+            <th>Valeur cible</th>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+          <tr>
+            <td>Nom actionneur</td>
+            <td>Récupérer valeur</td>
+            <td>
+              <form method="post" action="traitement.php">
+                <p>
+                  <select name="valeur" id="valeur">
+                    <option value="valeur 1">Valeur 1</option>
+                    <option value="valeur 2">Valeur 2</option>
+                    <option value="valeur 3">Valeur 3</option>
+                    <option value="valeur">valeur 4</option>
+                    <option value="valeur">valeur 5</option>
+                    <option value="valeur">valeur 6</option>
+                    <option value="valeur">valeur 7</option>
+                    <option value="valeur">valeur 8</option>
+                  </select>
+                </p>
+                </form>
+            </td>
+          </tr>
+        </table>
+    </div>
+    <div class="aide">
+      <img src="info.png" alt="aide concernant les actionneurs" title="Remplir ici le formulaire d'aide">
+    </div>
+  </div>
+
+</div>
+</body>
+
+</html>
