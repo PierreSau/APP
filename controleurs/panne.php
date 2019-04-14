abed0ad91f829a169fa0e54b0b065de0a7301d8d
Vincent TELLIER <43743447+Vintel92isep@users.noreply.github.com>
2019-04-14 14:56:26 +0200

    Fonctionnalité panne pour pres client

---
+++ controleurs/panne.php
@@ -0,0 +1,40 @@
+<?php
+
+include 'C:\Users\vince\APP\modele/panne.php';
+
+if(empty($_POST['choix'])){
+    include 'C:\Users\vince\APP\vues/panne.php'; //Vue de choix
+}
+else{
+
+    include 'C:\Users\vince\APP\vues/panne2.php'; //Vue de validation
+    ajoutePanne($bdd,$_POST['choix']);
+    //Parametrage de l'envoi du mail de confirmation
+    $header="MIME-Version: 1.0"."\n";
+    $header.='From:"SAV EcoM"<ConcactService.EcoM@gmail.com>'."\n";
+    $header.="Reply-to: \"EcoM\" <ConcactService.EcoM@gmail.com>"."\n";
+    $header.='Content-Type:text/html; charset="utf-8"'."\n";
+    $header.='Content-Transfer-Encoding: 8bit';
+
+    $message='
+<html>
+	<body>
+		<divalign="center">
+			Votre déclaration de panne a bien été prise en compte.</br>
+			Cordialement, </br>
+			Le SAV EcoM
+		</div>
+	</body>
+</html>
+';
+    $mail=recupererEmail($bdd);
+
+    $Objet='EcoM: Déclaration reçue';
+    mail($mail,$Objet, $message, $header);
+}
+
+
+
+
+
+
