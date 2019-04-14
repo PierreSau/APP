682365bc269ca2f767ff5e9a25fb5978a0a5e295
Vincent TELLIER <43743447+Vintel92isep@users.noreply.github.com>
2019-04-14 18:15:49 +0200

    Switch Case pour index

--- controleurs/panne.php
+++ controleurs/panne.php
@@ -1,22 +1,30 @@
 <?php
 
 include 'modele/panne.php';
-
 if(empty($_POST['choix'])){
-    include 'vues/panne.php'; //Vue de choix
+    $function = 'AccueilPanne';
 }
 else{
+    $function = 'ValidationChoix';
+}
 
-    include 'vues/panne2.php'; //Vue de validation
-    ajoutePanne($bdd,$_POST['choix']);
-    //Parametrage de l'envoi du mail de confirmation
-    $header="MIME-Version: 1.0"."\n";
-    $header.='From:"SAV EcoM"<ConcactService.EcoM@gmail.com>'."\n";
-    $header.="Reply-to: \"EcoM\" <ConcactService.EcoM@gmail.com>"."\n";
-    $header.='Content-Type:text/html; charset="utf-8"'."\n";
-    $header.='Content-Transfer-Encoding: 8bit';
+switch ($function){
+    case 'AccueilPanne':
+        $vue = 'panne';
+        $title = 'Déclaration d\'une panne';
+        break;
+    case 'ValidationChoix':
+        $vue = 'panne2';
+        $title = 'Problème déclaré';
+        ajoutePanne($bdd,$_POST['choix']);
+        //Parametrage de l'envoi du mail de confirmation
+        $header="MIME-Version: 1.0"."\n";
+        $header.='From:"SAV EcoM"<ConcactService.EcoM@gmail.com>'."\n";
+        $header.="Reply-to: \"EcoM\" <ConcactService.EcoM@gmail.com>"."\n";
+        $header.='Content-Type:text/html; charset="utf-8"'."\n";
+        $header.='Content-Transfer-Encoding: 8bit';
 
-    $message='
+        $message='
 <html>
 	<body>
 		<divalign="center">
@@ -27,11 +35,18 @@ else{
 	</body>
 </html>
 ';
-    $mail=recupererEmail($bdd);
+        $mail=recupererEmail($bdd);
 
-    $Objet='EcoM: Déclaration reçue';
-    mail($mail,$Objet, $message, $header);
+        $Objet='EcoM: Déclaration reçue';
+        mail($mail,$Objet, $message, $header);
+        break;
+
+
 }
+include('vues/header.php');
+include('vues/' . $vue . '.php');
+include('vues/footer.php');
+
 
 
 
