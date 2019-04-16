5665acc63c4f5896db83d379ad9051a2cc009439
henrimini <43743816+henrimini@users.noreply.github.com>
2019-04-16 13:40:57 +0200

    Update footer.php

--- vues/footer.php
+++ vues/footer.php
@@ -12,7 +12,7 @@
             <p> 10 rue de Vanves, <br> 92130 Issy-les-Moulineaux </p>
         </div>
         <div id="footer3">
-            <p><a href="index.php?cible=faq">FAQ</a></p>
+            <p><a href="index.php?cible=utilisateurs&fonction=faq">FAQ</a></p>
         </div>
         <div id="footer1">
             <p><a href="index.php?cible=vcontact">Contact</a></p>
