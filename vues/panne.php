2beccf721950165bf8bdc830b1c2095c096f8026
Vincent TELLIER <43743447+Vintel92isep@users.noreply.github.com>
2019-05-06 11:46:30 +0200

    oui
    oui

--- vues/panne.php
+++ vues/panne.php
@@ -1,11 +1,22 @@
+<?php
 
+?>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="stylepagepanne.css" />
+
+    <title>Déclaration d'une panne</title>
+</head>
 <body>
+<p>
     <h2>
     Pour déclarer votre problème, veuillez sélectionner la caractérique principale de ce problème parmi les solutions proposées ci dessous :
     </h2>
-
-<form method="POST" action="index.php?cible=panne&fonction=validationpanne" >
-    <select name="choix" value="choix" id="choix">
+</p>
+<P>
+<form method="POST" action="controlleurpanne.php">
+    <select name="choix" value="choix">
         <option value="pbCemac">Problème de connexion du CeMac</option>
         <option value="pbCapteur">Problème de connexion d'un capteur</option>
         <option value="valAbs">Valeurs absurdes</option>
@@ -14,7 +25,10 @@
     <input type="submit" value="Valider" />
 
 </form>
+</p>
+</body>
 
+</html>
 
 
 
