09dc956e49b6ac89e746e8f90d88b6d6717f6d01
PierreSau <pierre.sauvage@isep.fr>
2019-05-20 10:07:48 +0200

    réparation bordel SAUVAGE 0.3

--- modele/requetes.stat.php
+++ modele/requetes.stat.php
@@ -5,3 +5,37 @@
  * Date: 2019-05-20
  * Time: 10:05
  */
+
+include('requetes.generiques.php');
+
+function recupCapteur(PDO $bdd): array
+{
+    $query = "SELECT * FROM `captact`";;
+    //$sth->prepare("SELECT $ FROM table WHERE id = :idUtilisateur")
+    $sth = $bdd->prepare($query);
+    //$sth->bindValue(":idUtilisateur", $variablePHP)
+    $sth->execute();
+    $res = $sth->fetchAll();
+    return $res;
+}
+
+
+function recupPiece(PDO $bdd): array
+{
+    $query = "SELECT * FROM `piece`";;
+    //$sth->prepare("SELECT $ FROM table WHERE id = :idUtilisateur")
+    $sth = $bdd->prepare($query);
+    //$sth->bindValue(":idUtilisateur", $variablePHP)
+    $sth->execute();
+    $res = $sth->fetchAll();
+    return $res;
+}
+
+function calculConsoPiece(PDO $bdd,$idPiece){
+
+    $sth = $bdd->prepare("SELECT SUM(consommation) FROM `captact`WHERE idPiece = :idPiece");
+    $sth->bindValue(":idPiece", $idPiece);
+    $sth->execute();
+    $res = $sth->fetchAll();
+    return $res;
+}
